5828c181305c917627af664017043356cf04460b
Abhinay Verma <aman.ver56@gmail.com>
2024-09-26 12:28:25 +0530

    Q3. Repititions

---
+++ src/cses/Repititions.java
@@ -0,0 +1,26 @@
+package cses;
+
+import java.util.Scanner;
+
+public class Repititions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int cnt = 1, maxi = 0;
+		for(int i=1;i<s.length();i++) {
+			if(s.charAt(i) == s.charAt(i-1)) {
+				cnt++;
+			}
+			else {
+				maxi = Math.max(maxi, cnt);
+				cnt = 1;
+			}
+		}
+		maxi = Math.max(maxi, cnt);
+		System.out.println(maxi);
+		sc.close();
+	}
+
+}
